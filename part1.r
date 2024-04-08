d1e222683316e8d873d8c4551fcce4b2a4a39b40
ejmsona <samuel.b.jonsson@ericsson.com>
2024-04-08 11:58:30 +0200

    part 1 and 2 files created

---
+++ part1.r
@@ -0,0 +1,26 @@
+# Deﬁne the tested regions;
+
+# Create the observed signal with the vectorized pixels of
+# these three regions using windows of 20 × 20 pixels;
+
+# Check the data behavior to verify if the considered regression
+# models are suitable approaches to ﬁt such data;
+
+# Create two dummy covariates;
+
+# Fit the selected regression models. You can use the functions
+# available on Canvas.
+
+# Perform the detection theory. Are the covariates signiﬁcant to the
+# model? Are they introducing information about variations in y?
+
+# Test the residuals. Is the model correctly speciﬁed? (Consider a
+# residual vs index plot and check evidence of normality with a
+# histogram, for example).
+
+# Check the relationship between the mean of y and the dummy covariates.
+
+# Verify the determination coeﬃcient for the ﬁtted models.
+
+# In conclusion: what is the most accurate model for such data,
+# considering detection and modeling evaluation?
---
+++ part2.r
@@ -0,0 +1,17 @@
+# Split the data set in to test and validation part, separating the last
+# 10 observed values to evaluate the forecasting;
+
+# Check the data behavior to identify suitable approaches to ﬁt such data;
+# Fit the Gaussian-base and model based on suitable distributions. To ﬁt,
+# for example, the beta and Kumaraswamy-based ARMA models (βARMA
+# model and KARMA model, respectively), you can use the functions available
+# on Canvas.
+
+# Evaluate the ﬁtted models.
+
+# Test the residuals. Are the models suitable for the tested data sets?
+
+# Evaluate the prediction capability of the tested models in the test and
+# validation data sets.
+
+# In conclusion: what is the most accurate model for such data?
