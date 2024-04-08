6c286cc632fbc8969b36a813dc133a4f37a4741c
ejmsona <samuel.b.jonsson@ericsson.com>
2024-04-08 11:16:50 +0200

    test file update

--- test.r
+++ test.r
@@ -1,12 +1,11 @@
 k <- 0
-for (i in 1:50){
-    #print(paste("k is", k, ", i is", i, sep = " "))
-    k = k + i
+for (i in 1:50) {
+    k <- k + i
 }
 print(k)
 
-circumference <- function(radius=1) {
-    2*pi*radius
+circumference <- function(radius = 1) {
+    2 * pi * radius
 }
 
 for (i in 1:5) {
