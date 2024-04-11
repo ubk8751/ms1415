a2291924cd01c2d3d6943258d4d8ac5703017b48
ejmsona <samuel.b.jonsson@ericsson.com>
2024-04-11 15:42:45 +0200

    test regions defined?

--- part1.r
+++ part1.r
@@ -1,26 +1,51 @@
-# Deﬁne the tested regions;
+# Load data
+data <- read.csv("data/image_SF.csv")
 
-# Create the observed signal with the vectorized pixels of
-# these three regions using windows of 20 × 20 pixels;
+# Print some metadata to make sure file was read
+dims <- dim(data)
+print(dims)
 
-# Check the data behavior to verify if the considered regression
-# models are suitable approaches to ﬁt such data;
+itm <- data[1, 1]
+print(itm)
 
-# Create two dummy covariates;
 
-# Fit the selected regression models. You can use the functions
-# available on Canvas.
+# 1.  Deﬁne the tested regions;
+mA <- data[9:17, 14:21]
+colnames(mA) <- 14:21
+# print("A:")
+# print(mA)
 
-# Perform the detection theory. Are the covariates signiﬁcant to the
-# model? Are they introducing information about variations in y?
+mB <- data[8:16, 170:177]
+colnames(mB) <- 170:177
+# print("B:")
+# print(mB)
 
-# Test the residuals. Is the model correctly speciﬁed? (Consider a
-# residual vs index plot and check evidence of normality with a
-# histogram, for example).
+mC <- data[62:70, 112:119]
+colnames(mC) <- 112:119
+# print("C:")
+# print(mC)
 
-# Check the relationship between the mean of y and the dummy covariates.
+# 2.  Create the observed signal with the vectorized pixels of
+#     these three regions using windows of 20 × 20 pixels;
 
-# Verify the determination coeﬃcient for the ﬁtted models.
+# 3.  Check the data behavior to verify if the considered regression
+#     models are suitable approaches to ﬁt such data;
 
-# In conclusion: what is the most accurate model for such data,
-# considering detection and modeling evaluation?
+# 4.  Create two dummy covariates;
+
+# 5.  Fit the selected regression models. You can use the functions
+#     available on Canvas.
+
+# 6.  Perform the detection theory. Are the covariates signiﬁcant to the
+#     model? Are they introducing information about variations in y?
+
+# 7.  Test the residuals. Is the model correctly speciﬁed? (Consider a
+#     residual vs index plot and check evidence of normality with a
+#     histogram, for example).
+
+# 8.  Check the relationship between the mean of y and the dummy covariates.
+
+# 9.  Verify the determination coeﬃcient for the ﬁtted models.
+
+# 10. In conclusion: what is the most accurate model for such data,
+#     considering detection and modeling evaluation?
